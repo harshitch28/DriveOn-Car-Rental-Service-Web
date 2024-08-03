9dfb3b415e2804f1f23d41aadf8506dbb48b7dfe
harshitch28 <147797347+harshitch28@users.noreply.github.com>
2024-08-04 01:25:02 +0530

    Implementing ScrollReveal

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+const sr = ScrollReveal({
+    distance: '80px',
+    duration:2500,
+    delay : 400,
+    reset : true
+})
+
+sr.reveal('.text',{delay:200,origin:'top'})
+sr.reveal('.heading',{delay:300,origin:'top'})
+sr.reveal('.ride-container .box',{delay:300,origin:'bottom'})
+sr.reveal('.services-container .box',{delay:300,origin:'left'})
+sr.reveal('.about-container ',{delay:300,origin:'right'})
+sr.reveal('.reviews-container ',{delay:300,origin:'left'})
+sr.reveal('.news .box',{delay:200,origin:'right'})
